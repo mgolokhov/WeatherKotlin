6c9902d774953f461b83a4334329b34bc51f9e9c
Maxim Golokhov <hexwer@gmail.com>
2018-01-24 00:34:48 +0300

    add simple adapter

---
+++ app/src/main/java/org/dodroid/max/weatherkotlin/ForecastListAdapter.kt
@@ -0,0 +1,21 @@
+package org.dodroid.max.weatherkotlin
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+class ForecastListAdapter(private val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+            ViewHolder(TextView(parent.context))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder (val textView: TextView): RecyclerView.ViewHolder(textView)
+
+}
