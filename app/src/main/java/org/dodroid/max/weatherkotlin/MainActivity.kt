400293a3386254b0fee17693cddc14149d8252f3
Maxim Golokhov <hexwer@gmail.com>
2018-01-24 01:21:24 +0300

    make network call with the help of Anko doAsync

--- app/src/main/java/org/dodroid/max/weatherkotlin/MainActivity.kt
+++ app/src/main/java/org/dodroid/max/weatherkotlin/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.find
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +22,19 @@ class MainActivity : AppCompatActivity() {
             "Sun 6/29 - Sunny - 20/7"
     )
 
+    private val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
+            "APPID=15646a06818f61f7b8d7823ca833e1ce&q=94043&mode=json&units=metric&cnt=7"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
+        val forecastList: RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
+
+        doAsync {
+            Request(url).run()
+            uiThread { longToast("done") }
+        }
     }
 }
